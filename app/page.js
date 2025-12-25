9db59126235ad1a2cd094cee0bb70cc30dbee2e8
Md Ikbal <ikbal84336@gmail.com>
2025-12-25 22:30:30 +0530

    refactor: update User model and related files to remove name field and add clerkId, role, and timestamps
    - Updated User model in schema.prisma to replace name with clerkId and added role, firstName, lastName, imageUrl, createdAt, and updatedAt fields.
    - Modified migration.sql to drop name column and add new fields with appropriate constraints.
    - Adjusted generated types and filters in commonInputTypes.ts, enums.ts, and User.ts to reflect the new User model structure.
    - Removed prisma.config.ts as it was no longer needed.
    - Updated package.json to downgrade Prisma client version to match schema changes.

--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
 
+import { onBoardUser } from "@/modules/auth/actions";
 import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  await onBoardUser()
   return (
     <div className="flex flex-col justify-center items-center h-screen"> <UserButton /></div>
   );
---
+++ modules/auth/actions/index.js
@@ -0,0 +1,33 @@
+"use server"
+import { db } from "@/lib/db"
+import { currentUser } from "@clerk/nextjs/server"
+export const onBoardUser = async () => {
+    try {
+        const user = await currentUser()
+        if (!user) {
+            return { success: false, error: "No authentication user found" }
+        }
+        const { id, firstName, lastName, imageUrl, emailAddresses } = user
+        const newUser = await db.user.upsert({
+            where: {
+                clerkId: id
+            },
+            update: {
+                firstName: firstName || null,
+                lastName: lastName || null,
+                imageUrl: imageUrl || null,
+                email: emailAddresses[0]?.emailAddress || ""
+            }, create: {
+                clerkId: id,
+                firstName: firstName || null,
+                lastName: lastName || null,
+                imageUrl: imageUrl || null,
+                email: emailAddresses[0]?.emailAddress || ""
+            }
+        })
+        return { success: true, user: newUser, message: "user onBoard successfully" }
+    } catch (error) {
+        console.error("Error in onBoardUser:", error)
+        return { success: false, error: "Failed to onBoard user" }
+    }
+}
