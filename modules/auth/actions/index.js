7e00605688236f4409691a0bce0665772d47e996
Md Ikbal <ikbal84336@gmail.com>
2025-12-25 23:28:23 +0530

    feat: implement RootLayout and Home page with user role handling and navigation

--- modules/auth/actions/index.js
+++ modules/auth/actions/index.js
@@ -31,3 +31,24 @@ export const onBoardUser = async () => {
         return { success: false, error: "Failed to onBoard user" }
     }
 }
+
+export const currentUserRole = async () => {
+    try {
+        const user = await currentUser()
+        if (!user) {
+            return { success: false, error: "No authentication user found" }
+        }
+        const { id } = user
+        const userRole = await db.user.findUnique({
+            where: {
+                clerkId: id
+            }, select: {
+                role: true
+            }
+        })
+        return userRole.role
+    } catch (error) {
+        console.error("Error in currentUserRole:", error)
+        return { success: false, error: "Failed to fetch user role" }
+    }
+}
