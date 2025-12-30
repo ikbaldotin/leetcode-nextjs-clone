0b34d98bef85db41b9ed35bd39da05884b73e88c
Md Ikbal <ikbal84336@gmail.com>
2025-12-30 22:20:25 +0530

    feat: Add profile page with user data, stats, and submission history components

---
+++ app/(root)/profile/page.jsx
@@ -0,0 +1,36 @@
+import { getCurrentUserData } from "@/modules/profile/actions";
+import PlaylistsSection from "@/modules/profile/components/playlist-sections";
+import ProfileStats from "@/modules/profile/components/profile-statc";
+import SolvedProblems from "@/modules/profile/components/solved-problem";
+import SubmissionsHistory from "@/modules/profile/components/submission-history";
+import UserInfoCard from "@/modules/profile/components/user-info";
+
+import React from "react";
+
+const ProfilePage = async () => {
+  const profileData = await getCurrentUserData();
+
+  console.log(profileData);
+  return (
+    <div className="min-h-screen  py-32">
+      <div className="container mx-auto px-4 max-w-7xl">
+        <UserInfoCard userData={profileData} />
+
+        <ProfileStats
+          submissions={profileData.submissions}
+          solvedCount={profileData.solvedProblems.length}
+          playlistCount={profileData.playlists.length}
+        />
+
+        <SubmissionsHistory submissions={profileData.submissions} />
+
+        <div className="grid  gap-8">
+          <SolvedProblems solvedProblems={profileData.solvedProblems} />
+          <PlaylistsSection playlists={profileData.playlists} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProfilePage;
